94a6eca7aec49805caf23de6692199d59176fc82
hdfaouz <hdfaouzia2@gmail.com>
2025-08-07 12:19:53 +0100

    fonction d'ajouter Book

--- src/main/java/com/enaa/miniprojet/Controller/BookController.java
+++ src/main/java/com/enaa/miniprojet/Controller/BookController.java
@@ -1,4 +1,21 @@
 package com.enaa.miniprojet.Controller;
 
+import com.enaa.miniprojet.Dto.BookDto;
+import com.enaa.miniprojet.Service.BookService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
 public class BookController {
+
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @PostMapping
+    public BookDto ajouter(@RequestBody BookDto bookDto){
+        return bookService.ajouter(bookDto);
+    }
 }
--- src/main/java/com/enaa/miniprojet/Service/BookService.java
+++ src/main/java/com/enaa/miniprojet/Service/BookService.java
@@ -1,8 +1,28 @@
 package com.enaa.miniprojet.Service;
 
+import com.enaa.miniprojet.Dto.AuthorDto;
+import com.enaa.miniprojet.Dto.BookDto;
+import com.enaa.miniprojet.Mappers.BookMap;
+import com.enaa.miniprojet.Model.Author;
+import com.enaa.miniprojet.Model.Book;
+import com.enaa.miniprojet.Repository.BookRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookService {
 
+    private final BookMap bookMap;
+    private final BookRepository bookRepository;
+
+
+    public BookService(BookMap bookMap, BookRepository bookRepository) {
+        this.bookMap = bookMap;
+        this.bookRepository = bookRepository;
+    }
+
+    public BookDto ajouter(BookDto dto){
+        Book book = bookMap.toEntity(dto);
+        Book save = bookRepository.save(book);
+        return bookMap.toDto(save);
+    }
 }
