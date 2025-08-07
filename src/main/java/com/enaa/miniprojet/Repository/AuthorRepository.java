2d15d16edb618ef8a8a1848e68440833fb4d8132
hdfaouz <hdfaouzia2@gmail.com>
2025-08-07 12:49:11 +0100

    implementation des fonctionalité avec Query

--- src/main/java/com/enaa/miniprojet/Repository/AuthorRepository.java
+++ src/main/java/com/enaa/miniprojet/Repository/AuthorRepository.java
@@ -2,6 +2,12 @@ package com.enaa.miniprojet.Repository;
 
 import com.enaa.miniprojet.Model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface AuthorRepository extends JpaRepository<Author,Long> {
+    //3
+    @Query(value = "select * from Author where name LIKE 'A%'" ,nativeQuery=true)
+    List<Author> findAuthorsByName();
 }
--- src/main/java/com/enaa/miniprojet/Repository/BookRepository.java
+++ src/main/java/com/enaa/miniprojet/Repository/BookRepository.java
@@ -2,6 +2,20 @@ package com.enaa.miniprojet.Repository;
 
 import com.enaa.miniprojet.Model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book,Long> {
+
+    //2
+    @Query(value = "select * from Book where  date between ? and ?" ,nativeQuery=true)
+    List<Book> findBookByDat(Long dat1, Long dat2);
+
+    //4
+    @Query(value = "select * from Book order by Date desc" ,nativeQuery=true)
+    List<Book> findBookByDatDecr();
+    //5
+    @Query(value = "select count(*) from Book" ,nativeQuery=true)
+    Long findTotalBooks();
 }
