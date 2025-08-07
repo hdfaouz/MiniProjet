6e6e3352f8b5153c44e74a6158448de3d089565e
hdfaouz <hdfaouzia2@gmail.com>
2025-08-07 11:40:04 +0100

    creation de Dto

--- src/main/java/com/enaa/miniprojet/Dto/AuthorDto.java
+++ src/main/java/com/enaa/miniprojet/Dto/AuthorDto.java
@@ -1,4 +1,32 @@
 package com.enaa.miniprojet.Dto;
 
+
+
 public class AuthorDto {
+    private String name;
+    private String biography;
+
+    public AuthorDto() {
+    }
+
+    public AuthorDto(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
 }
--- src/main/java/com/enaa/miniprojet/Dto/BookDto.java
+++ src/main/java/com/enaa/miniprojet/Dto/BookDto.java
@@ -1,4 +1,42 @@
 package com.enaa.miniprojet.Dto;
 
+import com.enaa.miniprojet.Model.Author;
+
 public class BookDto {
+    private String title;
+    private String isbn;
+    private Author author;
+
+    public BookDto() {
+    }
+
+    public BookDto(String title, String isbn, Author author) {
+        this.title = title;
+        this.isbn = isbn;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
