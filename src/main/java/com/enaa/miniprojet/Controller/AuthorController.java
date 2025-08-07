b18388014a63eaf8c2c62110ecd8b0c69df13df7
hdfaouz <hdfaouzia2@gmail.com>
2025-08-07 12:05:59 +0100

    fonction d'ajouter Author

--- src/main/java/com/enaa/miniprojet/Controller/AuthorController.java
+++ src/main/java/com/enaa/miniprojet/Controller/AuthorController.java
@@ -1,4 +1,25 @@
 package com.enaa.miniprojet.Controller;
 
+import com.enaa.miniprojet.Dto.AuthorDto;
+import com.enaa.miniprojet.Service.AuthorService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/author")
 public class AuthorController {
+
+    private final AuthorService authorService;
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping
+    public AuthorDto ajouter(@RequestBody AuthorDto dto){
+        return authorService.ajouter(dto);
+    }
+
 }
--- src/main/java/com/enaa/miniprojet/Service/AuthorService.java
+++ src/main/java/com/enaa/miniprojet/Service/AuthorService.java
@@ -1,7 +1,26 @@
 package com.enaa.miniprojet.Service;
 
+import com.enaa.miniprojet.Dto.AuthorDto;
+import com.enaa.miniprojet.Mappers.AuthorMap;
+import com.enaa.miniprojet.Model.Author;
+import com.enaa.miniprojet.Repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthorService {
+
+    private final AuthorMap authorMap;
+    private final AuthorRepository authorRepository;
+
+
+    public AuthorService(AuthorMap authorMap, AuthorRepository authorRepository) {
+        this.authorMap = authorMap;
+        this.authorRepository = authorRepository;
+    }
+
+    public AuthorDto ajouter(AuthorDto dto){
+        Author author = authorMap.toEntity(dto);
+        Author save = authorRepository.save(author);
+        return authorMap.toDto(save);
+    }
 }
